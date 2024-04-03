fbd06f9f930d0224dfe2ef7696b34d1796280a5b
pawandarji98 <pawandarji98@gmail.com>
2024-04-04 00:38:43 +0530

    push

--- controllers/business-controller.js
+++ controllers/business-controller.js
@@ -4,31 +4,32 @@ const AppError = require("../utils/app-error");
 require('dotenv').config();
 
 exports.getListOfBusinesses = customRapper(async (req, res, next) => {
-  try {
-    const response = await apiAxios.get("/locations?TenantId=4&LocationId=3", {
-      headers: { 
-        'Accept': 'application/json', 
-        'Authorization': req.body.token
-      }
-    });
-  
-    const detailDataPromises = response.data.data.map(async (element) => {
-      const detailDataResponse = await apiAxios.get(`/businessInformation?TenantId=4&LocationId=${element.locationId}`, {
+    try {
+      let detailDataList = []
+      const response = await apiAxios.get("/locations?TenantId=4&LocationId=3", {
         headers: { 
           'Accept': 'application/json', 
           'Authorization': req.body.token
         }
       });
-      return detailDataResponse.data;
-    });
+
+
+      for(let element of response.data.data) {
+        const detailDataResponse = await apiAxios.get(`/businessInformation?TenantId=4&LocationId=${element.locationId}`, {
+          headers: { 
+            'Accept': 'application/json', 
+            'Authorization': req.body.token
+          }
+        });
+        detailDataList.push(detailDataResponse.data);
+      }
   
-    const detailDataList = await Promise.all(detailDataPromises);
-    
-    res.json({
-      status: 'success',
-      data: detailDataList
-    });
-  } catch (e) {
-    return next(new AppError(`Error while getting all businesses ${e}`, 500));
-  }
+      res.json({
+        status: 'success',
+        data: detailDataList
+      });
+  
+    } catch (e) {
+      return next(new AppError(`Error while getting all businesses ${e}`, 500));
+    }
   });
